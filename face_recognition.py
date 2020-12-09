985b828fbd1791f062f28a4e60a1f4e2b60dccfd
M-inghsin <62242940+M-inghsin@users.noreply.github.com>
2020-12-10 02:58:04 +0900

    Create face_recognition.py
    Python 3.9.0

---
+++ face_recognition.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+# Library: pip3 install opencv-python
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('face_detector.xml')
+
+# Read the input image
+img = cv2.imread('img_test.jpg')
+
+# Detect faces in the image
+faces = face_cascade.detectMultiScale(img, 1.1, 4)
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 250, 205), 2)
+
+# Export the result
+cv2.imwrite('img_test.png', img)
+print('Found {0} face(s)!'.format(len(faces)), '\nSuccessfully saved')
